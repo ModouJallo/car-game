c1d036548d8568801810bdc49305c37f17d9fa12
Hambalieu <hambalieujallow@gmail.com>
2022-09-11 11:20:03 -0700

    created a car control game

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# car-game
+# Car control Game
+
+
+- The user can input a command to either: 
+  - start a car 
+  - stop a car
+  - quit from the app
+
---
+++ app.py
@@ -0,0 +1,28 @@
+command = ""
+started = False
+while True: 
+    command = input("> ").lower()
+    if command == 'help':
+      print('''
+start- to start the car 
+stop - to stop the car 
+quit - to exit
+      ''')
+    elif command == 'start':
+      if started:
+        print('Car is already started')
+      else:
+        started = True  
+        print("Car Started.... Ready to go!")
+
+    elif command == 'stop':
+        if not started:
+          print("Car is already stopped!")
+        else:
+          started = False  
+          print("Car Stopped.")
+
+    elif command == 'quit':
+      break
+    else:
+        print(" I don't understand that...")  
